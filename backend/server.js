01b774cc18a0ea252db835e4f9c66109701b2425
xVlad <62362419+ReyvaldoLay@users.noreply.github.com>
2023-07-03 15:22:30 +1000

    set up cron job for every minute

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ const containerRoutes = require("./routes/containerRoutes");
 const storageRoutes = require("./routes/storageRoutes");
 const activityRoutes = require("./routes/activityRoutes");
 const subscriptionRoutes = require("./routes/subscriptionRoutes");
-const paymentRoutes = require('./routes/paymentRoutes')
+const paymentRoutes = require('./routes/paymentRoutes');
+const schedule = require('node-schedule');
+
+const Activity = require("./models/activityModel");
 
 connectDB();
 
@@ -41,4 +44,29 @@ app.use("/api/payment-sheet", paymentRoutes);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
+});
+
+const rule = new schedule.RecurrenceRule();
+let time = new Date();
+time.setSeconds(time.getSeconds() + 3);
+rule.second = time.getSeconds();
+//run it 3 sec after code starts ish
+
+const job = schedule.scheduleJob(rule, async function () {
+  const activities = await Activity.find({});
+  let curTime = new Date();
+
+  for (let i = 0; i < activities.length; i++) {
+    let a = new Date(activities[i].createdAt)
+    let b = new Date(activities[i].createdAt)
+
+    b.setMonth(a.getMonth() + 1)
+    a.setDate(a.getDate() + 14)
+
+    if (curTime.getTime() >= b.getTime()) {
+      console.log("fine is due");
+    } else if (curTime.getTime() >= a.getTime()) {
+      console.log("2 week reminder");
+    }
+  }
 });
